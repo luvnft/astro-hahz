275116a07e98576839e4525f78ad2d15ff4a7db2
The Wizard of Hahz <info@luvnft.com>
2024-11-19 21:47:33 -0500

    updated

--- src/consts.ts
+++ src/consts.ts
@@ -2,21 +2,21 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
+  TITLE: "HAHZ.LIVE",
   DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  AUTHOR: "Wizard of Hahz",
 }
 
 // Work Page
 export const WORK: Page = {
   TITLE: "Work",
-  DESCRIPTION: "Places I have worked.",
+  DESCRIPTION: "My resume.",
 }
 
 // Blog Page
 export const BLOG: Page = {
   TITLE: "Blog",
-  DESCRIPTION: "Writing on topics I am passionate about.",
+  DESCRIPTION: "Explaining my solutions.",
 }
 
 // Projects Page 
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "hahz@blkluv.org.com",
+    HREF: "mailto:hahz@blkluv.org.com",
   },
   { 
-    NAME: "Github",
-    ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    NAME: "TikTok",
+    ICON: "tiktok",
+    TEXT: "Wizard of Hahz",
+    HREF: "https://tiktok.com/@wizardofhahz"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "Hahz Terry",
+    HREF: "https://www.linkedin.com/in/hassan-terry/",
   },
   { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    NAME: "YouTube",
+    ICON: "youtube",
+    TEXT: "YouTube",
+    HREF: "https://youtube.com/@wizardofhahz",
   },
 ]
 
